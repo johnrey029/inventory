4c8a474dc782e1a01f993fdca6d38c3655ecf544
johnrey029 <johnreyabdon029@gmail.com>
2018-08-11 05:41:32 +0800

    Production

---
+++ ecci.inv.system/production/Production.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ecci.inv.system.production
+{
+    public partial class Production : System.Web.UI.MasterPage
+    {
+        private string sessionempno { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["empnumber"] != null)
+            {
+               // lblEmpno.Text = Session["empnumber"].ToString();
+            }
+            else
+            {
+                Session.Clear();
+                Response.Redirect("~/default.aspx");
+            }
+        }
+    }
+}
